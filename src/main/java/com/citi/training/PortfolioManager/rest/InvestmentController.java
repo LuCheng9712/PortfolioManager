89857fecdeefc7515067f2773132956d0b36423f
Lingjing Zou <lingjing.zou@mail.utoronto.ca>
2021-08-13 15:37:11 +0000

    change in repo

--- src/main/java/com/citi/training/PortfolioManager/rest/InvestmentController.java
+++ src/main/java/com/citi/training/PortfolioManager/rest/InvestmentController.java
@@ -1,4 +1,28 @@
 package com.citi.training.PortfolioManager.rest;
 
+
+import com.citi.training.PortfolioManager.entities.Investment;
+import com.citi.training.PortfolioManager.service.InvestmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/investments")
 public class InvestmentController {
+
+    @Autowired
+    private InvestmentService investmentService;
+
+
+    @GetMapping
+    public Collection<Investment> getAllInvestments() {
+        return investmentService.getAllInvestments();
+    }
+
+
+
 }
