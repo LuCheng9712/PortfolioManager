08bde31328d88ba3fadb205f37cc8897fff07ea6
Lingjing Zou <lingjing.zou@mail.utoronto.ca>
2021-08-13 16:55:29 +0000

    add invest and trans schemas

--- sql/portfolio_database.sql
+++ sql/portfolio_database.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `portfolio_database` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `portfolio_database`;
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: portfolio_database
 -- ------------------------------------------------------
@@ -18,6 +18,30 @@ USE `portfolio_database`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `cash_accounts`
+--
+
+DROP TABLE IF EXISTS `cash_accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cash_accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `amount` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cash_accounts`
+--
+
+LOCK TABLES `cash_accounts` WRITE;
+/*!40000 ALTER TABLE `cash_accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cash_accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `investments`
 --
 
@@ -26,6 +50,7 @@ DROP TABLE IF EXISTS `investments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `investments` (
   `id` int NOT NULL AUTO_INCREMENT,
+  `ticker` varchar(5) NOT NULL,
   `name` varchar(45) NOT NULL,
   `type` varchar(10) NOT NULL,
   `quantity` int NOT NULL,
@@ -41,8 +66,36 @@ CREATE TABLE `investments` (
 
 LOCK TABLES `investments` WRITE;
 /*!40000 ALTER TABLE `investments` DISABLE KEYS */;
-INSERT INTO `investments` VALUES (1,'tesla','stock',1,200);
+INSERT INTO `investments` VALUES (1,'','tesla','stock',1,200);
 /*!40000 ALTER TABLE `investments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `time` datetime NOT NULL,
+  `quantity` int DEFAULT NULL,
+  `type` varchar(45) NOT NULL,
+  `amount` double NOT NULL,
+  `account_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `account_id` FOREIGN KEY (`id`) REFERENCES `cash_accounts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transactions`
+--
+
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-13 15:38:40
+-- Dump completed on 2021-08-13 16:53:42
