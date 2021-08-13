3465de2c559a29c63c7feec159415f1e7e795fb1
kyleair <kyleair8@gmail.com>
2021-08-13 14:55:28 +0000

    created Investment.java entity

--- src/main/java/com/citi/training/PortfolioManager/entities/Investment.java
+++ src/main/java/com/citi/training/PortfolioManager/entities/Investment.java
@@ -1,4 +1,63 @@
 package com.citi.training.PortfolioManager.entities;
 
-public class Investment {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="investments")
+public class Investment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+    @Column(name="type")
+    private String type;
+    @Column(name="quantity")
+    private int quantity;
+    @Column(name="average_purchase_price")
+    private double avgPurchasePrice;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getAvgPurchasePrice() {
+        return avgPurchasePrice;
+    }
+
+    public void setAvgPurchasePrice(double avgPurchasePrice) {
+        this.avgPurchasePrice = avgPurchasePrice;
+    }
 }
