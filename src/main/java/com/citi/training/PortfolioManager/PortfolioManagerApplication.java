f6ec52d7e81f3f5a6234c3d11eea85d001bc49a3
UWLuCheng <lucheng9712@gmail.com>
2021-08-13 10:02:48 -0400

    Initial Commit

---
+++ src/main/java/com/citi/training/PortfolioManager/PortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.citi.training.PortfolioManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citi/training/PortfolioManager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.training.PortfolioManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
