ec07f76a7022b0d4b073367348eb5a3e66e46369
Lu (Lucy) Cheng <lucheng9712@gmail.com>
2021-08-13 14:47:09 +0000

    Updated InvestmentRepository

--- src/main/java/com/citi/training/PortfolioManager/repo/InvestmentRepository.java
+++ src/main/java/com/citi/training/PortfolioManager/repo/InvestmentRepository.java
@@ -1,4 +1,10 @@
 package com.citi.training.PortfolioManager.repo;
 
-public class InvestmentRepository {
+import com.citi.training.PortfolioManager.entities.Investment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface InvestmentRepository extends JpaRepository<Investment, Integer> {
+    Collection<Investment> findByName(String title);
 }
