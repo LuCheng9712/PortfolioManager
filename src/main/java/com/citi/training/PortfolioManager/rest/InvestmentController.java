70d26f509e06a2ebd79701c4b4ade40b1a1707b7
kyleair <kyleair8@gmail.com>
2021-08-23 20:23:33 +0000

    added crossorigin annotation to investments

--- src/main/java/com/citi/training/PortfolioManager/rest/InvestmentController.java
+++ src/main/java/com/citi/training/PortfolioManager/rest/InvestmentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/investments")
 public class InvestmentController {
 
